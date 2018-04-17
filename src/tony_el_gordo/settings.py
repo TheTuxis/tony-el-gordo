29972a7fb183e6ed69b25ae0775408ea998762b3
Pablo N Dalmasso <pablodalmasso@gmail.com>
2018-04-16 21:20:38 -0300

    #1 SeAgrega aplicacion, modelo y admin

--- src/tony_el_gordo/settings.py
+++ src/tony_el_gordo/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #====APPS?====
+    'loans'
 ]
 
 MIDDLEWARE = [
@@ -73,12 +75,12 @@ WSGI_APPLICATION = 'tony_el_gordo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
 
 
 # Password validation
@@ -118,3 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+try:
+    from .local_settings import *
+except:
+    pass
